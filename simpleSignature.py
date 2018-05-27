81a1cca5b5fc4611849bf79e6945dc4ccc22f9cb
gian michele innocenti <ginnocen@cern.ch>
2018-05-27 14:23:08 -0400

    working code with 2 variable optimisation

--- simpleSignature.py
+++ simpleSignature.py
@@ -21,8 +21,6 @@ from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 
-
-
 do1D=0
 do2D=0
 doSVM=1
@@ -31,14 +29,15 @@ h = .02  # step size in the mesh
 
 datasets= ["small sample"]
 
-names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
-         "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
-         "Naive Bayes", "QDA"]
+names = ["Nearest_Neighbors", "Linear_SVM", "RBF_SVM", "Gaussian_Process",
+         "Decision_Tree", "Random_Forest", "Neural_Net", "AdaBoost",
+         "Naive_Bayes", "QDA"]
 
 
 input_file = "msk_tune_for_gian2.csv"
+input_file_test = "msk_test.csv"
 df = pd.read_csv(input_file, header = 0)
-#df=df.loc[(df['pdau0'] <2.0) &  ((df['isparticleID0'] ==1) |  (df['isparticleID0'] ==2))]                                                    # create a dataframe after applying a selection e.g. here we are trying to select only pions 
+df_test= pd.read_csv(input_file_test, header = 0)
 
 bin_values = np.arange(start=-0.2, stop=1.2, step=0.03)
 
@@ -52,7 +51,6 @@ if(do1D == 1):
     plt.show()
     
 if(do2D == 1): 
-
     df_sig=df.loc[df['signal'] ==1]                              
     df_bkg=df.loc[df['signal'] ==0]                              
     plt.scatter(df_sig.Signature_3_c, df_sig.Signature_3_ml, s=20, c='b', label='signal')
@@ -69,10 +67,16 @@ df_features = df_selected.to_dict(orient='records')
 vec = DictVectorizer()
 X = vec.fit_transform(df_features).toarray()
 
+y_test = np.asarray(df_test.signal)
+df_selected_test = df_test.drop(['tumor', 'signal', "Signature_3_l_rat", "Signature_3_wl","rat_sig3","Signature_other_c4_ml","exp_sig3","total_snvs","Signature_3_c1_ml","Signature_3_c2_ml","Signature_3_c3_ml","Signature_3_c4_ml"], axis=1)
+df_features_test = df_selected_test.to_dict(orient='records')
+vec_test = DictVectorizer()
+X_test = vec.fit_transform(df_features_test).toarray()
+
 classifiers = [
     KNeighborsClassifier(3),
-    SVC(kernel="linear", C=0.025),
-    SVC(gamma=2, C=1),
+    SVC(kernel="linear", C=0.025, probability=True),
+    SVC(gamma=2, C=1,probability=True),
     GaussianProcessClassifier(1.0 * RBF(1.0)),
     DecisionTreeClassifier(max_depth=5),
     RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
@@ -81,7 +85,10 @@ classifiers = [
     GaussianNB(),
     QuadraticDiscriminantAnalysis()]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.4, random_state=42)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.4, random_state=42)
+X_train=X
+y_train=y
+
 x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
 y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
@@ -112,9 +119,17 @@ figure = plt.figure(figsize=(10,3))
 i=1
 for name, clf in zip(names, classifiers):
     ax = plt.subplot(1, len(classifiers) + 1, i)
-    clf.fit(X_train, y_train)
+    clf.fit(X_train, y_train,)
     score = clf.score(X_test, y_test)
-
+    y_test_prediction=clf.predict(X_test)
+    y_test_probability=clf.predict_proba(X_test)[:,1]
+    
+    df_test_pred=df_test
+    df_test_pred = df_test_pred.assign(estimated_decision=pd.Series(y_test_prediction))
+    df_test_pred = df_test_pred.assign(estimated_probability=pd.Series(y_test_probability))
+    nameoutput="cvspredictions"+names[i-1]+".csv"
+    df_test_pred.to_csv(nameoutput)
+    
     # Plot the decision boundary. For that, we will assign a color to each
     # point in the mesh [x_min, x_max]x[y_min, y_max].
     if hasattr(clf, "decision_function"):
@@ -137,6 +152,7 @@ for name, clf in zip(names, classifiers):
     ax.set_yticks(())
     ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'), size=15, horizontalalignment='right')
     i += 1
+    
 plt.show()
 
 
